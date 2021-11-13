9da92864741789dce7e08d9041cdf78e066fdd89
DiFei <346274777@qq.com>
2021-11-13 12:10:19 +0800

    创建个人组件文件夹

--- src/components/ZhouPan/test.vue
+++ src/components/ZhouPan/test.vue
@@ -1,15 +1,13 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-13 12:03:20
- * @LastEditTime: 2021-11-13 12:09:12
+ * @LastEditTime: 2021-11-13 12:09:57
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \win11_vue\src\components\HeDiFei\test.vue
 -->
 <template>
-  <div>
-  
-  </div>
+  <div></div>
 </template>
 
 <script>
@@ -19,5 +17,4 @@ export default {
 </script>
 
 <style>
-
 </style>
