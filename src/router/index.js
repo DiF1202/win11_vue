214bda38fe32c72c358edb496535844be14420d4
DiFei <346274777@qq.com>
2021-11-13 17:40:59 +0800

    更改了路由，使得点开默认出现的就是桌面，便于开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-11-13 14:06:23
- * @LastEditTime: 2021-11-13 15:19:50
- * @LastEditors: your name
+ * @LastEditTime: 2021-11-13 17:39:32
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \win11_vue\src\router\index.js
  */
@@ -11,17 +11,17 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    redirect: "/open",
-  },
-  {
-    path: "/open",
-    name: "open",
-    component: () => import("../views/open.vue"),
+    redirect: "/desktop",
   },
   {
     path: "/desktop",
     name: "desktop",
     component: () => import("../views/desktop.vue"),
+  },
+  {
+    path: "/open",
+    name: "open",
+    component: () => import("../views/open.vue"),
   },
 ];
 
