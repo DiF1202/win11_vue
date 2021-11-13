c13348b8dc740789c4c210e3c8f8a7cc73909f1b
DiFei <346274777@qq.com>
2021-11-13 17:38:55 +0800

    更改了desktop.vue里面的position定位错误

--- src/views/desktop.vue
+++ src/views/desktop.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-13 14:06:23
- * @LastEditTime: 2021-11-13 16:18:39
+ * @LastEditTime: 2021-11-13 17:37:53
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \win11_vue\src\views\desktop.vue
@@ -18,18 +18,12 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-html,
-body {
-  width: 100%;
-  height: 100%;
-}
 .desktop {
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-  position: absolute;
+  width: 100vw;
+  height: 100vh;
   background-image: url('../assets/img/wallpapers/light.jpg');
   background-position: center;
   background-size: cover;
