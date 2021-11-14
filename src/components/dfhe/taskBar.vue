2da9dc6b2f22ebbb1a10c4721a979ce0bebfb9d5
DiFei <346274777@qq.com>
2021-11-14 09:39:26 +0800

    文件名更改

--- src/components/dfhe/taskBar.vue
+++ src/components/dfhe/taskBar.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-13 12:03:20
- * @LastEditTime: 2021-11-13 22:10:42
+ * @LastEditTime: 2021-11-14 09:34:51
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \win11_vue\src\components\HeDiFei\test.vue
@@ -81,10 +81,10 @@ export default {
       display: flex;
       .taskDate {
         height: 100%;
-        width: 52px;
+        width: 55px;
         display: flex;
         padding: 3px 4px 4px 4px;
-        font-size: 0.64em;
+        font-size: 13px;
         flex-direction: column;
         margin-right: 15px;
         .dateDiv {
--- src/views/desktop.vue
+++ src/views/desktop.vue
@@ -1,31 +1,38 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-11-14 09:33:51
+ * @LastEditTime: 2021-11-14 09:38:35
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \win11_vue\src\views\desktop.vue
+-->
 <template>
   <div class="desktop">
     <div class="main" @contextmenu.prevent="rightClick">
       <!-- 鼠标右键出现的列表 -->
       <Click ref="clickRef" v-if="visClick"></Click>
       <!-- 桌面图标列表组件 -->
-      <AppList :displayMode="displayMode" :sortMethod="sortMethod"></AppList>
+      <AppList :displayMode="displayMode"></AppList>
     </div>
-    <taskBar></taskBar>
+    <TaskBar></TaskBar>
   </div>
 </template>
 
 <script>
-import taskBar from "../components/dfhe/taskBar.vue";
-import AppList from "../components/dssun/DesktopAppList.vue";
-import Click from "../components/panzhou/click.vue";
+import TaskBar from '../components/dfhe/TaskBar';
+import AppList from '../components/dssun/DesktopAppList.vue';
+import Click from '../components/panzhou/click.vue';
 export default {
-  name: "desktop",
+  name: 'desktop',
   components: {
-    taskBar,
+    TaskBar,
     AppList,
     Click,
   },
   data() {
     return {
       visClick: true,
-      displayMode: "small", // 控制桌面图标大小：small 小图标（默认） middle 中图标 big 大图标
-      sortMethod: "date", // 控制图标排序方式：size 按大小 date 按时间 name 按名称
+      displayMode: 'small', // 控制桌面图标大小：small 小图标（默认） middle 中图标 big 大图标
     };
   },
   methods: {
@@ -44,7 +51,7 @@ export default {
   justify-content: flex-end;
   width: 100vw;
   height: 100vh;
-  background-image: url("../assets/img/wallpapers/light.jpg");
+  background-image: url('../assets/img/wallpapers/light.jpg');
   background-position: center;
   background-size: cover;
   overflow: hidden;
