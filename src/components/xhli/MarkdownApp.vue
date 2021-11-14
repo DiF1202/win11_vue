a298fbdedb4e9b698b45c04aa89325d588612808
Tremere12 <1098646639@qq.com>
2021-11-14 20:31:02 +0800

    改了组件名称

--- src/components/xhli/MarkdownApp.vue
+++ src/components/xhli/MarkdownApp.vue
@@ -4,17 +4,18 @@
  * @LastEditTime: 2021-11-13 12:09:12
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \win11_vue\src\components\HeDiFei\test.vue
+ * @FilePath: \win11_vue\src\components\xhli\MarkdownApp.vue
 -->
 <template>
   <div>
-  
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'test',
+  name: 'MarkdownApp',
+
 };
 </script>
 
