462ff5a96e375a11407e6c56fdefd3c41b2e5440
sadose <sadose@qq.com>
2021-11-16 00:13:41 +0800

    修复了合并造成的代码丢失

--- src/components/dssun/DesktopAppList.vue
+++ src/components/dssun/DesktopAppList.vue
@@ -3,7 +3,7 @@
     <Item
       v-for="(ap, index) in sortedAppsList"
       :appName="ap.appName"
-      :imgUrl="require('@/assets/img/appIcons/' + ap.description + '.png')"
+      :imgUrl="imgUrl(ap.description)"
       :displayMode="displayMode"
       :key="index"
       @click="openApp"
@@ -30,6 +30,7 @@
 
 <script>
 import Item from "./DesktopAppItem.vue";
+import NewItem from "./DesktopNewItem.vue";
 
 export default {
   name: "desktop-app-list",
@@ -37,9 +38,11 @@ export default {
     displayMode: String, // 图标显示模式：big 大图标 middle 中图标 small 小图标
     sortMethod: String, // 图标排序方式：size 按大小 date 按时间 name 按名称
   },
-  components: { Item },
+  components: { Item, NewItem },
   data() {
     return {
+      ifNewFile: false,
+      newFileType: "txt",
       appsList: [
         {
           appName: "计算机",
@@ -105,6 +108,13 @@ export default {
     },
   },
   methods: {
+    imgUrl(imgName) {
+      if (imgName.indexOf("/") + 1) {
+        if (imgName.split("/")[0][0] === "t")
+          return require("@/assets/img/appIcons/txt.png");
+        else return require("@/assets/img/appIcons/folder.png");
+      } else return require("@/assets/img/appIcons/" + imgName + ".png");
+    },
     openApp(e) {
       let tar = e.target;
       let i = 0;
@@ -113,7 +123,35 @@ export default {
         ++i;
       }
       if (i >= 5) return;
-      this.$emit("winStateChange", tar.id, 4);
+      if (typeof this.appUrl[tar.id] == "undefined") return;
+      if (this.appUrl[tar.id]) window.open(this.appUrl[tar.id]);
+      else this.$emit("winStateChange", tar.id, 4);
+    },
+    createNewItem(fileType) {
+      this.ifNewFile = true;
+      this.newFileType = fileType;
+    },
+    newItemCallback(fileType, fileName) {
+      this.ifNewFile = false;
+      this.appsList.push({
+        appName: fileName,
+        description: fileType + "/" + fileName,
+        size: 1,
+        date: "2021-11-12",
+      });
+    },
+    // 测试函数
+    test1() {
+      this.createNewItem("folder");
+    },
+    test2() {
+      this.createNewItem("txt");
+    },
+    test3() {
+      if (this.displayMode === "small") this.$emit("changeDeskIconSize", 1);
+      else if (this.displayMode === "middle")
+        this.$emit("changeDeskIconSize", 2);
+      else this.$emit("changeDeskIconSize", 0);
     },
   },
 };
--- src/views/desktop.vue
+++ src/views/desktop.vue
@@ -8,6 +8,7 @@
         :displayMode="displayMode"
         :sortMethod="sortMethod"
         @winStateChange="winStateChange"
+        @changeDeskIconSize="changeDeskIconSize"
       ></AppList>
       <!-- Edge应用窗口 -->
       <EdgeApp
@@ -29,14 +30,14 @@
 </template>
 
 <script>
-import BarTask from '../components/dfhe/BarTask.vue';
-import AppList from '../components/dssun/DesktopAppList.vue';
-import Click from '../components/panzhou/click.vue';
-import EdgeApp from '../components/dssun/EdgeApp.vue';
-import VscodeApp from '../components/dssun/VscodeApp.vue';
+import BarTask from "../components/dfhe/BarTask.vue";
+import AppList from "../components/dssun/DesktopAppList.vue";
+import Click from "../components/panzhou/click.vue";
+import EdgeApp from "../components/dssun/EdgeApp.vue";
+import VscodeApp from "../components/dssun/VscodeApp.vue";
 
 export default {
-  name: 'desktop',
+  name: "desktop",
   components: {
     AppList,
     Click,
@@ -46,32 +47,32 @@ export default {
   },
   data() {
     return {
-      //#region  dss 控制的 data
-      displayMode: 'small', // 控制桌面图标大小：small 小图标（默认） middle 中图标 big 大图标
-      sortMethod: 'date', // 控制图标排序方式：size 按大小 date 按时间 name 按名称
+      //#region  dssun 控制的 data
+      displayMode: "small", // 控制桌面图标大小：small 小图标（默认） middle 中图标 big 大图标
+      sortMethod: "date", // 控制图标排序方式：size 按大小 date 按时间 name 按名称
       winMax: {
         // 窗口是否最大化：false 否 true 是
         // 通过改变对应 app 的该数组项来控制窗口的**最小化和显示**：
         // * true -> false : 从显示状态最小化
         // * false -> true : 从最小化状态显示窗口
-        edge: 'true',
-        vscode: 'true',
+        edge: "true",
+        vscode: "true",
       },
       winHide: {
         // 窗口是否隐藏：false 否 true 是
         // 通过改变对应 app 的该数组项来控制窗口的**打开和关闭**：
         // * true -> false : 从关闭状态到打开
         // * false -> true : 从打开状态到关闭
-        edge: 'true',
-        vscode: 'true',
+        edge: "true",
+        vscode: "true",
       },
       winSize: {
         // 窗口尺寸：normal 还原窗口 max 最大化窗口
         // 通过改变对应 app 的该数组项来控制窗口的**最大化和还原**：
         // * normal -> max : 从还原状态到最大化
         // * max -> normal : 从最大化状态到还原
-        edge: 'max',
-        vscode: 'max',
+        edge: "max",
+        vscode: "max",
       },
       //#endregion
     };
@@ -81,7 +82,7 @@ export default {
     // 1. 鼠标右键点击出现 小弹框
     rightClick(e) {
       const { clientX, clientY } = e;
-      this.$store.commit('setClick', {
+      this.$store.commit("setClick", {
         clientX,
         clientY,
         vis: true,
@@ -89,31 +90,44 @@ export default {
     },
     // 2. 鼠标左键点击关闭小弹框
     closeClick() {
-      this.$store.commit('setClick', {
+      this.$store.commit("setClick", {
         vis: false,
       });
     },
     //#endregion
 
-    // 组件监听事件
+    //#region dssun 回调函数
+
+    // 1 提供给**窗口子组件**的回调函数
+    // 请在窗口子组件的右上角三个按钮的事件函数中使用 $emit 调用该函数以调整窗口状态
     winStateChange(appname, e) {
-      // console.log(appname, e);
       // appname 应用名称的唯一标识符
       // e 事件编码：0 关闭按钮被按下 1 最小化按钮被按下 2 最大化/还原按钮被按下 3 任务栏图标被按下 4 桌面图标被按下
       if (e === 0) {
-        this.winHide[appname] = 'true';
+        this.winHide[appname] = "true";
       } else if (e === 1) {
-        this.winMax[appname] = 'false';
+        this.winMax[appname] = "false";
       } else if (e === 2) {
-        if (this.winSize[appname] === 'normal') this.winSize[appname] = 'max';
-        else this.winSize[appname] = 'normal';
+        if (this.winSize[appname] === "normal") this.winSize[appname] = "max";
+        else this.winSize[appname] = "normal";
       } else if (e === 3) {
-        if (this.winMax[appname] === 'false') this.winMax[appname] = 'true';
-        else this.winMax[appname] = 'false';
+        if (this.winMax[appname] === "false") this.winMax[appname] = "true";
+        else this.winMax[appname] = "false";
       } else {
-        this.winHide[appname] = 'false';
+        this.winHide[appname] = "false";
       }
     },
+
+    // 2 提供给**右键菜单**子组件的回调函数
+    // 请在右键菜单子组件的切换大中小图标的事件函数中使用 $emit 调用该函数以调整桌面图标大小
+    changeDeskIconSize(iconSize) {
+      // iconSize 要切换成的图标尺寸：0 小图标 1 中图标 2 大图标
+      if (iconSize === 0) this.displayMode = "small";
+      else if (iconSize === 1) this.displayMode = "middle";
+      else this.displayMode = "big";
+    },
+
+    //#endregion
   },
 };
 </script>
@@ -125,7 +139,7 @@ export default {
   justify-content: flex-end;
   width: 100vw;
   height: 100vh;
-  background-image: url('../assets/img/wallpapers/light.jpg');
+  background-image: url("../assets/img/wallpapers/light.jpg");
   background-position: center;
   background-size: cover;
   overflow: hidden;
