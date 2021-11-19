979867dd121579ac1bcc0892d21367ef596c48ed
unknown <panzhou5@iflytek.com>
2021-11-19 20:06:31 +0800

    [有点小问题我先提交一下吧]

--- src/utils/preLoad.js
+++ src/utils/preLoad.js
@@ -153,7 +153,6 @@ const imgs = [
     require('../assets/img/wallpapers/dianCiJun.jpg'),
     require('../assets/img/wallpapers/leisai.jpg'),
     require('../assets/img/wallpapers/light.jpg'),
-    require('../assets/img/wallpapers/xiaofei.png'),
     require('../assets/img/wallpapers/xinhui.jpg'),
     require('../assets/img/wallpapers/xinhui1.jpg'),
     require('../assets/img/wallpapers/xinhui2.jpg')
